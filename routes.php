4d40fbdb5fe6ec6db8e8cfec3113fe83a3bf45b7
rncs92 <123461096+rncs92@users.noreply.github.com>
2023-05-18 10:23:22 +0300

    Update

--- routes.php
+++ routes.php
@@ -6,7 +6,6 @@ return [
     ['GET', '/', [Controller::class, 'getPosts']],
     ['GET', '/post', [Controller::class, 'viewPost']],
     ['GET', '/post/{id:\d+}', [Controller::class, 'viewPost']],
-    ['GET', '/post/user/{id:\d+}', [Controller::class, 'getUsers']],
     ['GET', '/user', [Controller::class, 'getUsers']],
     ['GET', '/user/{id:\d+}', [Controller::class, 'getUsers']],
 ];
